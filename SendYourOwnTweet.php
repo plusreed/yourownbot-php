f4a8bf64665397e10f6ba10d959a15e7301e1456
plusreed <plusreed@users.noreply.github.com>
2014-08-16 17:40:25 -0400

    Update SendYourOwnTweet.php
    Added some more comments ok

--- SendYourOwnTweet.php
+++ SendYourOwnTweet.php
@@ -1,30 +1,34 @@
 <?php
+// SendYourOwnTweet.php
+// Made by PlusReed because I love @yourownbot and I was bored. :D
+// Please, keep my name in this. I have no license to this, but, please, no theft. :-(
+$word1 = array("Add", "your", "words", "here");
+$word2 = array("Add", "your", "words", "here");
+$rand_word1 = $word1[array_rand($word1, 1)];
+$rand_word2 = $word2[array_rand($word2, 1)];
 
-$word1 = array(); // 1st word to be tweeted. i.e: [Word1 your own Word2!]
-$word2 = array(); // 2nd word to be tweeted. i.e: [Word1 your own Word2!]
-$random_word1 = $word1[array_rand($word1, 1)];
-$random_word2 = $word2[array_rand($word2, 1)];
-
-// Include twitteroauth
 require_once('twitteroauth.php');
 
-// Set keys
-$consumerKey = 'APIKeyHere';
-$consumerSecret = 'APISecretHere';
-$accessToken = 'AccessTokenHere';
-$accessTokenSecret = 'AccessTokenSecretHere';
+// We need the keys of the application used for this!
+$consumerkey = "consumerkeyhere";
+$consumersecret = "consumersecrethere";
+$apikey = "apikeyhere";
+$apikeysecret = "apikeysecrethere";
 
-// Create object
+// Create a new object
 $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 
-// Set status message
-$tweetMessage =  $random_word1.' your own '.$random_word2.'!';
+// Randomized tweet
+$tweetMessage = $rand_word1.' your own '.$rand_word2.'!';
 
-// Check for 140 characters
-if(strlen($tweetMessage) <= 140)
-{
-    // Post the status message
-    $tweet->post('statuses/update', array('status' => $tweetMessage));
+// Make sure if its under 140 characters
+// usually this isn't usually happening if you keep it under 140 characters
+// Keep the first 80 characters and the second 40 characters
+
+if(strlen($tweetMessage) <= 140) {
+	// Post the tweet
+	$tweet->post('statuses/update', array('status' => $tweetMessage));
+
 }
 
 ?>
