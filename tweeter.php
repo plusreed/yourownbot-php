f3de8a418f1204d40d532d6d28d95cf925f36356
Reed <reed@plusreed.com>
2018-01-23 14:05:19 -0500

    General fixes, rename to tweeter.php
    - Only allow the script to run via the CLI
    - Rename some variables for consistency
    - Use simple syntax for string formatting on randomized tweet
    - Update script to allow usage of up to 280 characters

--- tweeter.php
+++ tweeter.php
@@ -1,9 +1,10 @@
 <?php
-// SendYourOwnTweet.php
-// A basic PHP-powered @yourownbot clone.
-// By PlusReed, minor improvements by +Sean.
-// Licensed under MIT License
+if (!PHP_SAPI == 'cli') { die(); } // Only allow the CLI to run this script.
 
+// tweeter.php
+// https://github.com/plusreed/yourownbot-php
+
+// TODO: Separate into config.ini.
 $word1 = array("Add", "your", "words", "here");
 $word2 = array("Add", "your", "words", "here");
 $rand_word1 = $word1[array_rand($word1, 1)];
@@ -11,26 +12,21 @@ $rand_word2 = $word2[array_rand($word2, 1)];
 
 require_once('twitteroauth.php');
 
-// We need the keys of the application used for this!
+// Keys here
 $consumerkey = "consumerkeyhere";
 $consumersecret = "consumersecrethere";
 $apikey = "apikeyhere";
-$apikeysecret = "apikeysecrethere";
+$apisecret = "apisecrethere";
 
 // Create a new object
 $tweet = new TwitterOAuth($consumerkey, $consumersecret, $apikey, $apikeysecret);
 
-// Randomized tweet
-$tweetMessage = $rand_word1.' your own '.$rand_word2.'!';
+// Randomize tweet
+$tweetMessage = '$rand_word1 your own $rand_word2!';
 
-// Make sure if its under 140 characters
-// usually this isn't usually happening if you keep it under 140 characters
-// Keep the first 80 characters and the second 40 characters
-
-if(strlen($tweetMessage) <= 140) {
+// Make sure generated tweet is under 280 characters
+if(strlen($tweetMessage) <= 280) {
 	// Post the tweet
 	$tweet->post('statuses/update', array('status' => $tweetMessage));
-
 }
-
 ?>
